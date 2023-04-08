af6fa37825b45b0ba687c0ba6472da2e8590e446
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-04-08 02:14:17 -0700

    Implement the game structure

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ Danny Nguyen
 A01353084
 """
 import random
-import tkinter as tk
 
 
 def make_board(rows: int, columns: int) -> dict:
@@ -20,28 +19,28 @@ def make_board(rows: int, columns: int) -> dict:
     :return: a dictionary that contains coordinates of the rooms in a tuple equivalent to the name of the room
 
     """
-    rooms = ['Micetras', 'Lagormiths', 'Beanloo', 'Fobinicca', 'Rioncersu']
+    rooms = ['Nakroth', 'Murad', 'Kriknak', 'Elsu', 'Raz', 'Arthur', 'Iggy', 'Xeniel', 'Valhein', 'Aleister']
     return {(row, column): random.choice(rooms) for row in range(rows) for column in range(columns)}
 
 
-def make_character() -> dict:
+def make_character(name) -> dict:
     """
     Make the character.
 
     A function that creates a character, with the starting coordinates and HP.
 
-    :return: a dictionary that contains "X-coordinate" and "Y-coordinate" are equal to 0, "Current HP" is equal to 5
+    :return: a dictionary that contains "Floor" and "Room" are equal to 0, "Current HP" is equal to 5
 
     >>> player = make_character()
     >>> player
-    {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    {'Floor': 0, 'Room': 0, 'Current HP': 5}
 
     >>> user = make_character()
     >>> user
-    {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    {'Floor': 0, 'Room': 0, 'Current HP': 5}
 
     """
-    return {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    return {'Name': name, 'Floor': 4, 'Room': 0, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30}
 
 
 def describe_current_location(board: dict, character: dict) -> None:
@@ -54,13 +53,27 @@ def describe_current_location(board: dict, character: dict) -> None:
     :param character: a dictionary
     :precondition: board must be a dictionary that is created by make_board function
     :precondition: character must be a dictionary that is created by make_character function
-    :post-condition: define and describe the row, colum, room, and HP level the character at
+    :post-condition: define and describe the row, column, room, and HP level the character at
 
     """
-    x_place = character["X-coordinate"]
-    y_place = character["Y-coordinate"]
-    hp = character["Current HP"]
-    print(f"You're at row {y_place+1}, column {x_place+1}, The {board[(x_place, y_place)]}, with {hp} HP remaining.")
+    name = character["Name"]
+    floor = character["Floor"]
+    room = character["Room"]
+    hp = character["Health"]
+    level = character["Level"]
+    next_level = character["EXP To Level Up"]
+    print(f"{name} is at floor {floor+1}, room {room+1}, The {board[(floor, room)]},"
+          f" with {hp} health point(s) remaining, and {next_level} experience points to level up."
+          f"You are level {level}")
+
+
+def level_up(character: dict) -> dict:
+    character["Level"] += 1
+    character["Max Health"] = 5 + character["Level"]
+    character["EXP To Level Up"] = 20 * character["Level"]
+    if character["Health"] <= 5:
+        character["Health"] = 5
+    return character
 
 
 def get_user_choice() -> str:
@@ -73,8 +86,8 @@ def get_user_choice() -> str:
 
     """
     while True:
-        user_input = (input("Direction to move:\n1. [<] West\n2. [^] North\n3. [v] South\n4. [>] East\n")).lower()
-        if user_input in ("north", "n", "south", "s", "east", "e", "west", "w", "1", "2", "3", "4"):
+        user_input = (input("Direction to move:\n1. [<] Left\n2. [^] Up\n3. [v] Down\n4. [>] Right\n")).lower()
+        if user_input in ("up", "w", "down", "s", "right", "d", "left", "a", "1", "2", "3", "4"):
             break
         else:
             print(f"'{user_input}' is not defined as a valid key to move.\n")
@@ -109,16 +122,16 @@ def validate_move(character: dict, direction: str) -> bool:
     False
 
     """
-    if character["X-coordinate"] == 0 and direction in ("west", "w", "1"):
+    if character["Floor"] == 4 and direction in ("up", "w", "2"):
         return False
-    elif character["X-coordinate"] == 4 and direction in ("east", "e", "4"):
+    elif character["Floor"] == 0 and direction in ("down", "s", "3"):
         return False
-    elif character["Y-coordinate"] == 0 and direction in ("north", "n", "2"):
+    elif character["Room"] == 0 and direction in ("left", "a", "1"):
         return False
-    elif character["Y-coordinate"] == 4 and direction in ("south", "s", "3"):
+    elif character["Room"] == 4 and direction in ("right", "d", "4"):
         return False
     else:
-        if direction in ("north", "n", "south", "s", "east", "e", "west", "w", "1", "2", "3", "4"):
+        if direction in ("up", "w", "down", "s", "right", "d", "left", "a", "1", "2", "3", "4"):
             return True
         else:
             return False
@@ -141,29 +154,29 @@ def move_character(character: dict, direction: str) -> dict:
     >>> player_direction = "s"
     >>> after_player_move = move_character(player_move, player_direction)
     >>> after_player_move
-    {'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 5}
+    {'Floor': 0, 'Room': 1, 'Current HP': 5}
 
     >>> user_move = make_character()
     >>> user_direction = "e"
     >>> after_user_move = move_character(user_move, user_direction)
     >>> after_user_move
-    {'X-coordinate': 1, 'Y-coordinate': 0, 'Current HP': 5}
+    {'Floor': 1, 'Room': 0, 'Current HP': 5}
 
     >>> tester_move = make_character()
     >>> tester_direction = "s"
     >>> later_tester_move = move_character(tester_move, tester_direction)
     >>> later_tester_move
-    {'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 5}
+    {'Floor': 0, 'Room': 1, 'Current HP': 5}
 
     """
-    if direction in ("north", "n", "2"):
-        character["Y-coordinate"] -= 1
-    elif direction in ("south", "s", "3"):
-        character["Y-coordinate"] += 1
-    elif direction in ("east", "e", "4"):
-        character["X-coordinate"] += 1
-    elif direction in ("west", "w", "1"):
-        character["X-coordinate"] -= 1
+    if direction in ("up", "w", "2"):
+        character["Floor"] += 1
+    elif direction in ("down", "s", "3"):
+        character["Floor"] -= 1
+    elif direction in ("right", "d", "4"):
+        character["Room"] += 1
+    elif direction in ("left", "a", "1"):
+        character["Room"] -= 1
     return character
 
 
@@ -179,43 +192,50 @@ def check_if_goal_attained(character: dict) -> bool:
     :return: a boolean: True if goal attained; False if not
 
     >>> win_player = make_character()
-    >>> win_player["X-coordinate"] = 4
-    >>> win_player["Y-coordinate"] = 4
+    >>> win_player["Floor"] = 4
+    >>> win_player["Room"] = 4
     >>> check_win = check_if_goal_attained(win_player)
     Congratulations! You reached your destination!
     >>> check_win
     True
 
     >>> not_win_player = make_character()
-    >>> not_win_player["X-coordinate"] = 1
-    >>> not_win_player["Y-coordinate"] = 2
+    >>> not_win_player["Floor"] = 1
+    >>> not_win_player["Room"] = 2
     >>> check_not_win = check_if_goal_attained(not_win_player)
     >>> check_not_win
     False
 
     """
-    if character["X-coordinate"] == 4 and character["Y-coordinate"] == 4:
+    if character["EXP To Level Up"] <= 0:
+        level_up(character)
+    if character["Floor"] == 0 and character["Room"] == 4 and character["Level"] >= 3:
         print(f"Congratulations! You reached your destination!")
         return True
     else:
         return False
 
 
-def check_for_foes() -> bool:
+def check_for_challenge() -> bool:
     """
-    Check for foes.
+    Check for challenge.
 
-    A function that checks if foes is in the room or not.
+    A function that checks if challenge is in the room or not.
 
-    :post-condition: define and randomly assign foes
-    :return: a boolean: True if there are foes; False if not
+    :post-condition: define and randomly assign challenge
+    :return: a boolean: True if there are challenge; False if not
 
     """
-    number = random.randint(1, 5)
+    number = random.randint(1, 2)
     if number == 1:
         return True
     else:
         return False
+
+
+def caution_one_point(character):
+    if character["Health"] == 1:
+        print(f"WARNING: If you get one more wrong guess, you will lose the game! Be careful.\n")
 
 
 def guessing_game(character: dict, upper: int) -> None:
@@ -232,17 +252,18 @@ def guessing_game(character: dict, upper: int) -> None:
 
     """
     secret_number = str(random.randint(1, upper))
-    print("Opponents ahead! Guess the correct number to beat them.\n")
-    if character["Current HP"] == 1:
-        print("BE CAREFUL! If you guess this one wrong, you will lose the game!")
+    print(f"Opponents ahead! Guess the correct number to beat them.\n")
+    caution_one_point(character)
     guess = input(f"Enter a number from 1 to {upper}: ")
     if guess == secret_number:
+        character["EXP To Level Up"] -= 10
+        if character["Health"] < character["Max Health"]:
+            character["Health"] += 1
         print(f"You're right! It's {guess}.\n")
     else:
         print(f"It's {secret_number}. You guessed it wrong! Losing 1 HP.\n")
-        character["Current HP"] -= 1
-        if character["Current HP"] == 1:
-            print(f"WARNING: If you get one more wrong guess, you will lose the game! Be careful.\n")
+        character["Health"] -= 1
+        caution_one_point(character)
 
 
 def is_alive(character: dict) -> bool:
@@ -268,7 +289,7 @@ def is_alive(character: dict) -> bool:
     False
 
     """
-    if character["Current HP"] <= 0:
+    if character["Health"] <= 0:
         return False
     else:
         return True
@@ -280,9 +301,14 @@ def game() -> None:
     """
     rows = 5
     columns = 5
+    upper = 1
     board = make_board(rows, columns)
-    print(f"Welcome to the game!\nYou need to move to row {rows}, column {columns} to win!\n")
-    character = make_character()
+    print(f"--------------------------------------------------------------------------------------------------------"
+          f"\n\n\n"
+          f"Welcome to the game!\n"
+          f"You need to beat the boss at floor 1 room 5 after you reach level 3 to win!\n")
+    name = input('Enter your user name: ')
+    character = make_character(name)
     achieved_goal = False
     while not achieved_goal and is_alive(character):
         describe_current_location(board, character)
@@ -290,25 +316,21 @@ def game() -> None:
         valid_move = validate_move(character, direction)
         if valid_move:
             move_character(character, direction)
-            there_is_a_challenger = check_for_foes()
+            there_is_a_challenger = check_for_challenge()
             if there_is_a_challenger:
-                guessing_game(character, 4)
+                guessing_game(character, upper)
             achieved_goal = check_if_goal_attained(character)
         else:
             print(f"That's not a valid move. Please try again!\n")
     if not is_alive(character):
-        print("You lost the game!")
+        print(f"You lost the game!")
 
 
 def main():
     """
     Drives the program.
     """
-    # greeting = tk.Entry(fg="yellow", bg="blue", width=50)
 
-    # window = tk.Tk(greeting)
-
-    # greeting.pack()
     game()
 
 
