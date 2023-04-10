dfdc664356901e891e94ec9bedf219b73eede54c
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-04-10 04:19:59 -0700

    Implement unit tests for describe_current_location function in game.py

--- test_describe_current_location.py
+++ test_describe_current_location.py
@@ -22,7 +22,7 @@ class TestDescribeSimpleLocation(TestCase):
     def test_describe_second_location(self, mock_stdout):
         first_board = {(1, 3): 'Murad'}
         first_character = {'EXP To Level Up': 30, 'Floor': 1, 'Health': 5, 'Level': 1, 'Level One Games':
-            ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number'],
+                           ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number'],
                            'Max Health': 5, 'Name': 'Danny', 'Room': 3}
         describe_current_location(first_board, first_character)
         expected_output = "\nInformation about Danny: \nFloor: 2 |  Room: 4 |  Room Name: Murad | Health Point: 5 " \
@@ -33,11 +33,9 @@ class TestDescribeSimpleLocation(TestCase):
     def test_describe_third_location(self, mock_stdout):
         first_board = {(0, 4): 'Kriknak'}
         first_character = {'EXP To Level Up': 30, 'Floor': 0, 'Health': 5, 'Level': 1, 'Level One Games':
-            ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number'],
+                           ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number'],
                            'Max Health': 5, 'Name': 'Danny', 'Room': 4}
         describe_current_location(first_board, first_character)
         expected_output = "\nInformation about Danny: \nFloor: 1 |  Room: 5 |  Room Name: Kriknak | Health Point: 5 " \
                           "|  Level: 1 |  EXP To Level Up: 30 \n\n"
         self.assertEqual(mock_stdout.getvalue(), expected_output)
-
-
