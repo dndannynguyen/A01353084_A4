45d0273c0715244378e52c69fe7cbd6d95fbaed2
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-04-10 03:32:35 -0700

    Finish docstrings and doctests for functions in game.py

--- game.py
+++ game.py
@@ -21,6 +21,7 @@ def make_board(rows: int, columns: int) -> dict:
     :precondition: columns must be a positive integer greater than or equal to two
     :post-condition: define, make the board correctly, and randomly assign the name for each existed room
     :return: a dictionary that contains coordinates of the rooms in a tuple equivalent to the name of the room
+    :raise ValueError: if rows or columns or both are not positive integer
 
     """
     rooms = ['Nakroth', 'Murad', 'Kriknak', 'Elsu', 'Raz', 'Arthur', 'Iggy', 'Xeniel', 'Valhein', 'Aleister']
@@ -31,17 +32,23 @@ def make_character(name) -> dict:
     """
     Make the character.
 
-    A function that creates a character, with the starting coordinates and HP.
+    A function that creates a character, with information about character.
 
+    :param name: a string
+    :precondition: name must be a string
+    :post-condition: correctly creates character information
     :return: a dictionary that contains information about the character
+    :raises ValueError: when name is not a string
 
-    >>> player = make_character()
+    >>> player = make_character("Danny")
     >>> player
-    {'Floor': 0, 'Room': 0, 'Current HP': 5}
+    {'Name': 'Danny', 'Floor': 4, 'Room': 0, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30, \
+'Level One Games': ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number']}
 
-    >>> user = make_character()
+    >>> user = make_character('Python')
     >>> user
-    {'Floor': 0, 'Room': 0, 'Current HP': 5}
+    {'Name': 'Python', 'Floor': 4, 'Room': 0, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30, \
+'Level One Games': ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number']}
 
     """
     return {'Name': name, 'Floor': 4, 'Room': 0, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30,
@@ -57,9 +64,11 @@ def describe_current_location(board: dict, character: dict) -> None:
 
     :param board: a dictionary
     :param character: a dictionary
-    :precondition: board must be a dictionary that is created by make_board function
-    :precondition: character must be a dictionary that is created by make_character function
-    :post-condition: define and describe the row, column, room, and HP level the character at
+    :precondition: board must be a dictionary that has key "Room Name"
+    :precondition: character must be a dictionary that has key from the information
+    :post-condition: describe the character's current location and information
+    :raises ValueError: when board or character or both are not dictionaries
+    :raises KeyError: if board or character or both do not have the required key(s)
 
     """
     name = character["Name"]
@@ -79,6 +88,27 @@ def describe_current_location(board: dict, character: dict) -> None:
 
 
 def level_up(character: dict) -> dict:
+    """
+    Level up the character.
+
+     A function that levels up the character.
+
+    :param character: a dictionary
+    :pre-condition: character must have the required keys
+    :post-condition: define components to level up and level up the character correctly
+    :return: a new dictionary describes character after leveled up
+    :raises ValueError: when character is not a dictionary
+    :raises KeyError: if character does not have the required key(s)
+
+    >>> player = {'Name': 'Danny', 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 10}
+    >>> level_up(player)
+    {'Name': 'Danny', 'Max Health': 7, 'Health': 5, 'Level': 2, 'EXP To Level Up': 40}
+
+    >>> user = {'Name': 'Python', 'Max Health': 8, 'Health': 2, 'Level': 3, 'EXP To Level Up': 10}
+    >>> level_up(user)
+    {'Name': 'Python', 'Max Health': 9, 'Health': 5, 'Level': 4, 'EXP To Level Up': 80}
+
+    """
     character["Level"] += 1
     character["Max Health"] = 5 + character["Level"]
     character["EXP To Level Up"] = 20 * character["Level"]
@@ -113,22 +143,24 @@ def validate_move(character: dict, direction: str) -> bool:
 
     :param character: a dictionary
     :param direction: a string
-    :precondition: character must be created by make_character function
-    :precondition: direction must be a string returned by get_user_choice function
+    :precondition: character must have 'Floor' as a key
+    :precondition: direction must be a string
     :post-condition: define a valid move correctly
     :return: a boolean: True if direction is a valid move; False if direction is not a valid move
+    :raises ValueError: if character is not a dictionary and/or direction is not a string
+    :raises KeyError: if character does not have a key "Floor"
 
-    >>> valid_move_player = make_character()
+    >>> valid_move_player = make_character("ValidMover")
 
-    >>> first_direction = 'n'
+    >>> first_direction = '2'
     >>> validate_move(valid_move_player, first_direction)
     False
 
-    >>> second_direction = 's'
+    >>> second_direction = '4'
     >>> validate_move(valid_move_player, second_direction)
     True
 
-    >>> third_direction = 'w'
+    >>> third_direction = '1'
     >>> validate_move(valid_move_player, third_direction)
     False
 
@@ -156,28 +188,25 @@ def move_character(character: dict, direction: str) -> dict:
 
     :param character: a dictionary
     :param direction: a string
-    :precondition: character must be a dictionary that is generated by make_character function
-    :precondition: direction must be a string returned by get_user_choice function
+    :precondition: character must be a dictionary that has the required keys
+    :precondition: direction must be a string that is a valid move
     :post-condition: define and move the character's coordinates correctly
     :return: the new character dictionary after adjusting the character coordinates
+    :raises ValueError: if character is not a dictionary and/or direction is not a string
+    :raises KeyError: if character does not have a key "Floor"
 
-    >>> player_move = make_character()
-    >>> player_direction = "s"
+    >>> player_move = make_character("Player")
+    >>> player_direction = "4"
     >>> after_player_move = move_character(player_move, player_direction)
     >>> after_player_move
-    {'Floor': 0, 'Room': 1, 'Current HP': 5}
-
-    >>> user_move = make_character()
-    >>> user_direction = "e"
+    {'Name': 'Player', 'Floor': 4, 'Room': 1, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30, \
+'Level One Games': ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number']}
+    >>> user_move = make_character("User")
+    >>> user_direction = "3"
     >>> after_user_move = move_character(user_move, user_direction)
     >>> after_user_move
-    {'Floor': 1, 'Room': 0, 'Current HP': 5}
-
-    >>> tester_move = make_character()
-    >>> tester_direction = "s"
-    >>> later_tester_move = move_character(tester_move, tester_direction)
-    >>> later_tester_move
-    {'Floor': 0, 'Room': 1, 'Current HP': 5}
+    {'Name': 'User', 'Floor': 3, 'Room': 0, 'Max Health': 5, 'Health': 5, 'Level': 1, 'EXP To Level Up': 30, \
+'Level One Games': ['riddle', 'word', 'number'], 'Level Two Games': ['riddle', 'word_a', 'word_b', 'number']}
 
     """
     if direction in ("up", "w", "2"):
@@ -195,27 +224,14 @@ def check_if_goal_attained(character: dict) -> bool:
     """
     Check if goal attained.
 
-    A function that checks if user reaches row 2 column 2.
+    A function that checks if user reaches floor 1, room 5 and finish the challenge.
 
     :param character: a dictionary
     :precondition: character must be a dictionary returned by make_character function
     :post-condition: define goal attained correctly
     :return: a boolean: True if goal attained; False if not
-
-    >>> win_player = make_character()
-    >>> win_player["Floor"] = 4
-    >>> win_player["Room"] = 4
-    >>> check_win = check_if_goal_attained(win_player)
-    Congratulations! You reached your destination!
-    >>> check_win
-    True
-
-    >>> not_win_player = make_character()
-    >>> not_win_player["Floor"] = 1
-    >>> not_win_player["Room"] = 2
-    >>> check_not_win = check_if_goal_attained(not_win_player)
-    >>> check_not_win
-    False
+    :raises ValueError: if character is not a dictionary
+    :raises KeyError if character does not have required keys
 
     """
     if character["EXP To Level Up"] <= 0:
@@ -253,26 +269,41 @@ def check_for_challenge() -> bool:
 
 
 def caution_one_point(character: dict) -> None:
+    """
+    Give a warning.
+
+    A function that gives player a warning.
+
+    :param character: a dictionary
+    :precondition: character must have the key 'Health'
+    :post-condition: give user a warning at the right time
+    :raises ValueError: if character is not a dictionary
+    :raises KeyError: if character does not have the key 'Health'
+
+    """
     if character["Health"] == 1:
-        print(f"WARNING: If you get one more wrong guess, you will lose the challenge! Be careful.\n")
+        print(f"WARNING: If you get one more wrong guess, you will lose the game! Be careful.\n")
 
 
 def challenge_in_game(character: dict) -> None:
     """
-    A challenge in challenge.
+    A challenge.
 
-    A function that creates a challenge that requires user to guess a random number generated.
+    A function that give a challenge that matches character's level.
 
     :param character: a dictionary
-    :precondition: character must be a dictionary that is returned by make_character function
-    :precondition: upper must be a non-zero positive integer
+    :precondition: character must have required keys
     :post-condition: define and run the challenge correctly
+    :raises ValueError: if character is not a dictionary
+    :raises KeyError: if character does not have the required keys
 
     """
     caution_one_point(character)
     if character["Level"] == 1:
-        win = level_one_games(random.choice(character["Level One Games"]))
+        game_choose_one = random.choice(character["Level One Games"])
+        win = level_one_games(game_choose_one)
         if win:
+            character["Level One Games"].remove(game_choose_one)
             character["EXP To Level Up"] -= 10
             if character["Health"] < character["Max Health"]:
                 character["Health"] += 1
@@ -280,8 +311,10 @@ def challenge_in_game(character: dict) -> None:
             character["Health"] -= 1
 
     elif character["Level"] == 2:
-        win = level_two_games(random.choice(character["Level Two Games"]))
+        game_choose_two = random.choice(character["Level Two Games"])
+        win = level_two_games(game_choose_two)
         if win:
+            character["Level Two Games"].remove(game_choose_two)
             character["EXP To Level Up"] -= 10
             if character["Health"] < character["Max Health"]:
                 character["Health"] += 1
@@ -307,14 +340,16 @@ def is_alive(character: dict) -> bool:
     :precondition: character must be a dictionary made from make_character function
     :post-condition: define alive correctly
     :return: a boolean: True if character is alive; False if character is not alive
+    :raises ValueError: if character is not a dictionary
+    :raises KeyError: if character does not have the key 'Health'
 
-    >>> alive_player = make_character()
+    >>> alive_player = make_character("Alive")
     >>> test_alive_player = is_alive(alive_player)
     >>> test_alive_player
     True
 
-    >>> dead_player = make_character()
-    >>> dead_player["Current HP"] = 0
+    >>> dead_player = make_character("Dead")
+    >>> dead_player["Health"] = 0
     >>> test_dead_player = is_alive(dead_player)
     >>> test_dead_player
     False
@@ -336,7 +371,9 @@ def game() -> None:
     print(f"--------------------------------------------------------------------------------------------------------"
           f"\n\n\n"
           f"Welcome to the challenge!\n"
-          f"You need to beat the boss at floor 1 room 5 after you reach level 3 to win!\n")
+          f"You need to finish the final challenge at floor 1 room 5 after you reach level 3 to win!\n\n"
+          f"(TIPS: Try not to lose mini-challenge. The more health points you have will bring you advantages"
+          f"at the final challenge!)\n\n")
     name = input('Enter your user name: ')
     character = make_character(name)
     achieved_goal = False
@@ -353,7 +390,7 @@ def game() -> None:
         else:
             print(f"That's not a valid move. Please try again!\n")
     if not is_alive(character):
-        print(f"You lost the challenge!")
+        print(f"You lost the game!")
 
 
 def main():
